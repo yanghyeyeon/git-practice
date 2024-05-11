19830df186fb3d10a1d15c0b94a1aed5682ddd28
yanghyeyeon <yhy4058@gmail.com>
2024-05-11 22:47:29 +0900

    출력 문자 변경

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("한글은 안 깨져요");
+        System.out.println("집에 가고 싶어요");
     }
 }
